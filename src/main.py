37a2fcaee614d7ca51b8b9bab125bbec985502c9
flaccoo <koopa.romero@gmail.com>
2022-02-15 14:17:29 +0100

    Added Characters

---
+++ src/main.py
@@ -0,0 +1,4 @@
+import rescources
+
+if __name__ == "__main__": 
+    pass
---
+++ src/rescources.py
@@ -0,0 +1,20 @@
+class Character: 
+
+    def __init__(self, name, health, attack, armor): 
+        self.name = name
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+    
+    def __str__(self) -> str:
+        return f"Name: {self.name}\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
+
+class Goblin: 
+
+    def __init__(self, health, attack, armor): 
+        self.health = health
+        self.attack = attack
+        self.armor = armor
+    
+    def __str__(self) -> str:
+        return f"Goblin\nHealth: {self.health}\nAttack: {self.attack}\nArmor: {self.armor}"
