ffa29422ea09b4e8b4d5837c93ce556a1b7f3327
flaccoo <koopa.romero@gmail.com>
2022-02-22 15:02:21 +0100

    save and load works fr now

--- src/main.py
+++ src/main.py
@@ -2,24 +2,23 @@ import resources
 from resources import Character, Goblin, save_character, load_characters
 from random import randint, shuffle, choice
 
-def fight(players : list, enemies : list ):
-    participants = players + enemies # create initiative order
-    
+def fight(players : list, enemies : list):
+    participants = players + enemies # skapa initiativordning 
     shuffle(participants)
-
+    
     for char in participants:
         target = ""
-        # is the character a player or a goblin
-        if char in players: 
+        # är karaktären en spelare eller en goblin?
+        if char in players:
             target = choice(enemies)
         else:
             target = choice(players)
-        
+            
         target.take_damage(char.get_attack())
-
-        if target.get_health() == 0: 
-            print(f"{target.get_name()} has died! ")
-            if type (target) == Goblin:
+        
+        if target.get_health() == 0:
+            print(f"{target.get_name()} has died!")
+            if type(target) == Goblin:
                 enemies.remove(target)
             else:
                 players.remove(target)
@@ -29,34 +28,38 @@ def fight(players : list, enemies : list ):
         
         if len(enemies) == 0 or len(players) == 0:
             break
-
-
+            
+    
 if __name__ == "__main__":
     enemies = []
-    players = []
-    nemy = Character("Nemy", 20, 5, 2)
-    nick = Character("Nick", 15, 2, 1)
-    players.append(nemy)
-    players.append(nick)
-
+    players = load_characters()
+    #players = []
+    
+    # emy = Character("Emy", 20, 5, 2)
+    # nick = Character("Nick", 15, 2, 1)
+    # players.append(emy)
+    # players.append(nick)
+    
     enemies.append(Goblin(10, 3, 2, 1))
     enemies.append(Goblin(15, 2, 1, 2))
     enemies.append(Goblin(12, 3, 1, 3))
-
-    round = 1 
+    
+    round = 1
     while len(enemies) != 0 and len(players) != 0:
         print(f"ROUND {round}, FIGHT!")
         fight(players, enemies)
         print()
         round +=1
-
+    
     if len(enemies) == 0:
-        print("The Players won!")
-        print(" w   ould you like to save the remaining characters?")
+        print("The players won!")
+        print("Would you like to save the remaining characters?")
         save_progress = input("(y/n): ")
         if save_progress == "y":
-            for player in players:
-                save_character(player)
+            save_character(players)
+        else:
+            print("No progress was saved.")
+        
     elif len(players) == 0:
         print("The Goblins won!")
     
--- src/resources.py
+++ src/resources.py
@@ -14,21 +14,20 @@ class Character:
         if relative_damage > 0:
             self.health -= relative_damage
         if self.health < 0: self.health = 0
-
-    def get_attack(self): # previously damage
+    
+    def get_attack(self): # tidigare damage
         return self.attack
 
     def get_health(self):
         return self.health
-
+    
     def get_name(self):
         return self.name
-
+    
     def get_attributes(self):
         return self.name, self.health, self.attack, self.armor
 
 class Goblin:
-    
     
     def __init__(self, health, attack, armor, id):
         self.health = health
@@ -50,26 +49,32 @@ class Goblin:
     
     def get_attack(self):
         return self.attack
-
+    
     def get_name(self):
         return f"Goblin #{self.id}"
+    
 
-def save_character(char : Character):
-    name, health, attack, armor = char.get_attributes()
-    save_string = f"{name}/{health}/{attack}/{armor}\n"
-    with open("saved_characters.txt", "a", encoding="utf8") as f:
-        f.write(save_string)
-        print(f"{name} has been succesfully saved. ")
+def save_character(chars : list()):
+    save_list = []
+    for character in chars:
+        name, health, attack, armor = character.get_attributes()
+        save_string = f"{name}/{health}/{attack}/{armor}\n"
+        save_list.append(save_string)
+        
+    with open("saved_characters.txt", "w", encoding="utf8") as f:
+        for line in save_list:
+            f.write(line)
+        print("Characters has been saved!")
 
 def load_characters():
     characters = []
     with open("saved_characters.txt", "r", encoding="utf8") as f:
         for line in f.readlines():
             attributes = line.split("/")
-            char = Character(attributes[0], 
-                             attributes[1], 
-                             attributes[2], 
-                             attributes[3])
-
+            char = Character(attributes[0],
+                             int(attributes[1]),
+                             int(attributes[2]),
+                             int(attributes[3]))
+            
             characters.append(char)
-    return characters
+    return characters    
